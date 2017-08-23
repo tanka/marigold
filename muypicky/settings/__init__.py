9cdf161d7c724c793b84fec56771c7461229a180
TNSharma <sharma.tankanath@gmail.com>
2017-08-23 14:07:57 +0600

    First Commit

---
+++ muypicky/settings/__init__.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# from .production import *
+print(BASE_DIR)
+
+try:
+	from .local import *
+except:
+	pass
---
+++ spare_parts/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
+
+# Create your views here.
+
+def test_home(request):
+
+	html_ = """
+
+<html lang="en-US">
+<head>   
+<title>Bhutan Power Corporation Limited</title>
+</head>
+
+<body class="home page-template page-template-page-home page-template-page-home-php page page-id-6 group-blog">
+
+    
+        
+</body>
+</html>
+
+"""
+	return HttpResponse(html_)
+
+def home(request):
+
+	return render(request, "base.html", {"value", True})
+
+class ContactView(View):
+	def get(self, request, *args, **kwargs):
+		
+		context = {}
+		return render(request, "contact.html", context)
